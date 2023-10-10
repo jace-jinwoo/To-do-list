974df1acddee379e1f8ca0e55ff084bee65b262d
jin <jwpark@gmail.com>
2023-10-10 17:20:35 +0900

    [feat] DB 연결 및 쿼리 실행 테스트

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const mysql = require('mysql2')
+const dbconfig = require('./database.js')
+const connection = mysql.createConnection(dbconfig)
+
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send("Hello"))
+// connection.connect()
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.set('port', process.env.PORT || 3000)
+app.get('/', (req, res) => {
+    console.log("req:: ", req.url)
+    let query = "SELECT * FROM TODOS"
+    // let query = "INSERT INTO todos (todo, due) VALUES ('test1', '231231');"
+    
+    connection.query(query, (err, results, fields) => {
+        if (err) console.log('err :: ', err)
+        res.send(results)
+        console.log('results :: ', results)
+    })
+})
+
+// connection.end()
+
+app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`))
