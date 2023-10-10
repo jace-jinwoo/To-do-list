b161801968f12fb22a5a0fa78df755042b82c7e8
jin <jwpark@gmail.com>
2023-10-10 17:50:02 +0900

    [feat] Register api 추가

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const mysql = require('mysql2')
 const dbconfig = require('./database.js')
+const bodyParser = require('body-parser')
 const connection = mysql.createConnection(dbconfig)
 
 const app = express()
 
-// connection.connect()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.set('port', process.env.PORT || 3000)
 app.get('/', (req, res) => {
-    console.log("req:: ", req.url)
     let query = "SELECT * FROM TODOS"
-    // let query = "INSERT INTO todos (todo, due) VALUES ('test1', '231231');"
     
     connection.query(query, (err, results, fields) => {
         if (err) console.log('err :: ', err)
@@ -20,6 +20,20 @@ app.get('/', (req, res) => {
     })
 })
 
-// connection.end()
+app.post('/register', (req, res) => {
+    
+    console.log("body :: ", req.body)
+    if (req.body?.id && req.body?.password) {
+        let query = `INSERT INTO todos (todo, due) VALUES ('${req.body?.id}', '${req.body?.password}');`
+        connection.query(query, (err, results, fields) => {
+            if (err) console.log('err :: ', err)
+            res.status(200).json({
+                success: true 
+            })
+            console.log('results :: ', results)
+        })
+    }
+    
+})
 
 app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`))
