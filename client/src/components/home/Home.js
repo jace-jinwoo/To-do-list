91858e78ddfe5a304f31444ec262fde2afff2311
jin <jwpark@gmail.com>
2023-10-13 14:37:48 +0900

    [feat] CORS - proxy 사용하여 처리

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 import React, { useEffect } from 'react'
 
-const axiosInstance = axios.create({baseURL: 'http://localhost:5000'})
+// const axiosInstance = axios.create({baseURL: 'http://localhost:5000'})
 
 function Home () {
     
     const callApi = React.useCallback(async () => {
-        const result = await axiosInstance.get('api/hello')
+        const result = await axios.get('/api/hello')
         console.log("Result :: ", result)
     }, [])
     
